36657f93119995aa93c4c5b9bc47c5a06d64f74e
Nick Dambrosio <ndambro303@gmail.com>
2018-05-14 15:39:45 -0600

    Add basic link button functionality

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var counter= 0;
 var enterButton = document.querySelector(".enter-button");
 enterButton.addEventListener("click", function(event){
   event.preventDefault();
+  createField();
   console.log('Enter button firing');
   // generateNewCard() function call goes here;
   document.querySelector(".number-of-links").innerText = counter;
@@ -43,3 +44,38 @@ function toggleReadLink(){
   element.classList.toggle("card-marked-as-read");
   addOneToReadCounter();
 }
+
+/* functionality */
+var websiteTitleInput = document.querySelector('.website-title-field');
+var websiteUrlInput = document.querySelector('.website-url-field');
+var enterButton = document.querySelector('.enter-button');
+var generatedField = document.querySelector('.website-box-mockup');
+
+
+websiteTitleInput.addEventListener('keyup', enterButton);
+websiteUrlInput.addEventListener('keyup', enterButton);
+enterButton.addEventListener('click', createField);
+
+
+function createField() {
+    siteName = (websiteTitleInput.value);
+    siteUrl = (websiteUrlInput.value);
+    generatedField.innerHTML = (`
+    <div class='website-box-mockup'>
+    <h2>${siteName}</h2>
+    <hr> 
+    <a href="${siteUrl}">
+      <p>${siteUrl}/p>
+    </a>
+    <hr>
+    <p class="bottom-text">Read</p>
+    <p class="bottom-text bottom-text2">Delete</p>
+    </div>`);
+    
+  };
+
+    // var deleteButton = document.querySelector('.bottom-text-delete');
+    // deleteButton.addEventListener('click', generatedField)
+    // console.log('delete-button-firing')
+    // event.preventDefault();
+    // ;
