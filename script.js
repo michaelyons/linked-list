c90303f13c1d69a15c165249b3c229974ac87b20
Nick D'Ambrosio <34214595+imsteaky@users.noreply.github.com>
2018-05-13 21:59:33 -0600

    Remove overflow, remove hard-coded heights, add basic JS functionality, fix styling

---
+++ script.js
@@ -0,0 +1,45 @@
+//function to clear the link counter
+var clearButton = document.querySelector(".clear-all-links");
+clearButton.addEventListener("click", clearAllLinks);
+
+function clearAllLinks(){
+  document.querySelector(".number-of-links").innerText = 0;
+  document.querySelector(".number-of-read").innerText = 0;
+  console.log("clear button firing");
+}
+
+// function to add a card to the counter and generated a new card when the enter button is clicked
+var counter= 0;
+var enterButton = document.querySelector(".enter-button");
+enterButton.addEventListener("click", function(event){
+  event.preventDefault();
+  console.log('Enter button firing');
+  // generateNewCard() function call goes here;
+  document.querySelector(".number-of-links").innerText = counter;
+  counter++;
+})
+
+
+// function to toggle the "read" class on all the cards. still needs to change the "read" text to red and any other styles that need to be applied
+var readLinkToggleButton = document.querySelector(".bottom-text-read");
+var readCounter = 0;
+
+function addOneToReadCounter(){
+  document.querySelector(".number-of-read").innerText = readCounter;
+  console.log('read counter firing');
+  readCounter++;
+}
+
+
+readLinkToggleButton.addEventListener("click", function(){
+  event.preventDefault();
+  console.log('toggle button firing');
+  toggleReadLink();
+})
+
+function toggleReadLink(){
+  console.log("toggle class function firing");
+  var element = document.querySelector(".card-mockup");
+  element.classList.toggle("card-marked-as-read");
+  addOneToReadCounter();
+}
