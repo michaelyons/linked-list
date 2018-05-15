c10408cb013abf79d6724b96f25408bb60448eb1
Nick Dambrosio <ndambro303@gmail.com>
2018-05-15 17:44:49 -0600

    Refactor lots.

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 var websiteTitleInput = document.querySelector(".website-title-field");
 var websiteUrlInput = document.querySelector(".website-url-field");
 var enterButton = document.querySelector(".enter-button");
-var generatedField = document.querySelector(".card-mockup");
-var deleteButton = document.querySelector(".delete-button");
+var generatedField = document.querySelector(".card-container");
 var counter = 0;
+var inputForm = document.querySelector(".form-fields")
 
 websiteTitleInput.addEventListener("keyup", enterButton);
 websiteUrlInput.addEventListener("keyup", enterButton);
@@ -16,8 +16,7 @@ function createField() {
   siteName = websiteTitleInput.value;
   siteUrl = websiteUrlInput.value;
   generatedField.innerHTML += `
-    <li id="card-list">
-      <article class="card-mockup">
+      <article class="card">
           <h2>${siteName}</h2>
           <hr>
           <a href="${siteUrl}">
@@ -26,54 +25,55 @@ function createField() {
           <hr>
           <button class="read-button">Read</button>
           <button class="delete-button">Delete</button>
-        </article>
-    </li>`;
-  var deleteButton = document.querySelector(".delete-button");
-  deleteButton.addEventListener("click", reset);
-  clearInputFields();
+        </article>`;
+  // var deleteButton = document.querySelector(".delete-button");
+  // deleteButton.addEventListener("click", reset);
+  inputForm.reset();
+  handleEnterButton();
+  newDeleteFunction();
 }
 
-var enterButton = document.querySelector(".enter-button");
-enterButton.addEventListener("click", function(event) {
-  event.preventDefault();
-  document.querySelector(".number-of-links").innerText = counter;
-  counter++;
-});
+function newDeleteFunction() {
+  var deleteButton = document.querySelectorAll(".delete-button");
+  console.log(deleteButton.length)
+  deleteButton[deleteButton.length - 1].addEventListener("click", deleteLinkButton);
+}
+//figure out how to select each card
 
 function deleteLinkButton() {
-  generatedField.innerHTML = "";
-  clearInputFields();
-  --counter;
+  // generatedField.innerHTML = "";
+  // clearInputFields();
+  // counter--;
+  console.log(this.parentNode)
+  this.parentNode.parentNode.removeChild();
+
 }
 
-function clearInputFields() {
-  generatedField.value = "";
+function handleEnterButton() {
+  document.querySelector(".number-of-links").innerText = counter;
+  counter++;
 }
 
-function reset() {
-  generatedField.innerHTML = "";
-  clearInputFields();
-}
 
 //------------------- READ BUTTON --------------------------//
 
-var readButton = document.querySelector(".read-button");
-var readCounter = 0;
+// var readButton = document.querySelector(".read-button");
+// var readCounter = 0;
 
-readButton.addEventListener("click", function() {
-  toggleReadLink();
-});
+// readButton.addEventListener("click", function() {
+//   toggleReadLink();
+// });
 
-function addOneToReadCounter() {
-  document.querySelector(".number-of-read").innerText = readCounter;
-  readCounter++;
+// function addOneToReadCounter() {
+//   document.querySelector(".number-of-read").innerText = readCounter;
+//   readCounter++;
 
-  function toggleReadLink() {
-    var element = document.querySelector(".card-mockup");
-    element.classList.toggle("card-marked-as-read");
-    addOneToReadCounter();
-  }
-}
+//   function toggleReadLink() {
+//     var element = document.querySelector(".card-mockup");
+//     element.classList.toggle("card-marked-as-read");
+//     addOneToReadCounter();
+//   }
+// }
 // ----------------------CLEAR ALL READ LINKS----------------------//
 
 var clearButton = document.querySelector(".clear-all-links");
