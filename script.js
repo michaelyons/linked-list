0c55d4e96d0fe020efec347958b84307d2afae24
Michael J. Lyons <mikelyons7@me.com>
2018-05-16 16:51:19 -0600

    final commit

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 var websiteTitleInput = document.querySelector(".website-title-field");
 var websiteUrlInput = document.querySelector(".website-url-field");
-var enterButton = document.querySelector(".enter-button");
-var generatedField = document.querySelector(".card-container");
+var enterButton = $(".enter-button");
+var generatedField = $(".card-container");
 var counter = 0;
-var inputForm = document.getElementById("form-fields");
+var readCounter = 0;
 var card = $(".card");
 
 websiteTitleInput.addEventListener("keyup", enterButton);
 websiteUrlInput.addEventListener("keyup", enterButton);
-enterButton.addEventListener("click", createField);
-generatedField.addEventListener("click", deleteLinkButton);
+enterButton.on("click", createField);
+generatedField.on("click", deleteLinkButton);
 
-
-
-// ---------------- enter button functionality------------- //
 
 function createField() {
   event.preventDefault();
   siteName = websiteTitleInput.value;
   siteUrl = websiteUrlInput.value;
-  $('.card-container').append(`
+  $('.card-container').prepend(`
       <article class="card">
           <h2>${siteName}</h2>
           <hr>
@@ -30,10 +27,8 @@ function createField() {
           <button class="read-button">Read</button>
           <button class="delete-button">Delete</button>
         </article>`);
-  inputForm.reset();
   handleEnterButton();
-
-}
+};
 
 function deleteLinkButton(event) {
   if (event.target.classList.contains("delete-button")) {
@@ -41,58 +36,36 @@ function deleteLinkButton(event) {
       counter--;
       $(".number-of-links").text(counter);
   }  
-}
+};
 
 function handleEnterButton() {
   counter++;
   $(".number-of-links").text(counter);
-}
+};
 
+$('.right-side').on('click', '.read-button', function() {
+  $(this).parent().toggleClass('card-marked-as-read');
+});
 
-//------------------- READ BUTTON --------------------------//
+function disableButtons(){
+  enterButton.disabled = true;
+};
 
-// var readButton = document.querySelector(".read-button");
-// var readCounter = 0;
+$('.website-title-field').on("keyup", action);
+function action() {
+    if($('.website-title-field').val().length > 0) {
+     $('.enter-button').prop("disabled", false);
+  }else {
+     $('.enter-button').prop("disabled", true);
+     disableButtons();
+  }
+};
 
-// readButton.addEventListener("click", function() {
-//   toggleReadLink();
-// });
-
-// function addOneToReadCounter() {
-//   document.querySelector(".number-of-read").innerText = readCounter;
-//   readCounter++;
-
-//   function toggleReadLink() {
-//     var element = document.querySelector(".card-mockup");
-//     element.classList.toggle("card-marked-as-read");
-//     addOneToReadCounter();
-//   }
-// }
-// ----------------------CLEAR ALL READ LINKS----------------------//
-
-var clearButton = document.querySelector(".clear-all-links");
-clearButton.addEventListener("click", clearAllLinks);
-
-function clearAllLinks() {
-  (document.querySelector(".number-of-links").innerText = 0), (counter = 0);
-  (document.querySelector(".number-of-read").innerText = 0), (readCounter = 0);
-}
-
-//pseudo code. doesn't actually do anything yet. This code will display an error if the user doesn't fill in the fields properly.
-
-// function noTextEntered(event){
-//   event.preventDefault();
-//   if(titlefield.value == "" && urlField.value == ""){
-//     alert("please fill in all the fields");
-//   }
-//   else if (titleField.value == ""){
-//     alert("fill in the title field");
-//   }
-//   else if (urlField.value == "");
-//     alert("fill in the url field");
-// } else {
-//   createCard();
-// }
-
-/* indicate a time stamp*/
-/* Date.now , set this somewhere on our template literal with vanilla or jquery*/
+$('.website-url-field').on("keyup", action);
+function action() {
+    if($('.website-url-field').val().length > 0) {
+     $('.enter-button').prop("disabled", false);
+  }else {
+     $('.enter-button').prop("disabled", true);
+  }
+};
